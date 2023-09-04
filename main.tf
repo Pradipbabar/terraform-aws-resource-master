8a35d47e63b1a3abeaf9073778a30a38bcc1477f
pradipbabar <pradipbaba13@gmail.com>
2023-09-04 13:11:14 +0530

    main

--- main.tf
+++ main.tf
@@ -1,25 +1,25 @@
 module "my_vpc" {
-  source = "pradipbabar/resource-master/aws-vpc"
+  source = "./modules/vpc"
     count = var.enable_vpc ? 1 : 0
   # VPC configuration here...
 }
 
 module "my_ec2_instances" {
-  source = "pradipbabar/resource-master/aws-ec2"
+  source = "./modules/ec2"
   count = var.enable_ec2 ? 1 : 0
 
   # EC2 instances configuration here...
 }
 
 module "my_rds_instance" {
-  source = "pradipbabar/resource-master/aws-rds"
+  source = "./modules/rds"
   count = var.enable_rds ? 1 : 0
 
   # RDS instance configuration here...
 }
 
 module "my_s3_bucket" {
-  source = "pradipbabar/resource-master/aws-s3"
+  source = "./modules/s3"
   count = var.enable_s3 ? 1 : 0
 
   # S3 bucket configuration here...
--- output.tf
+++ output.tf
@@ -1,23 +1,23 @@
-output "vpc_outputs" {
+output "vpc_id" {
   description = "Outputs from the VPC module"
-  value       = module.my_vpc[*].vpc_outputs  # Replace with actual module output variable names
+  value       = module.my_vpc[*].vpc_id  # Replace with actual module output variable names
   depends_on  = [module.my_vpc]
 }
 
-output "ec2_outputs" {
+output "ec2_id" {
   description = "Outputs from the EC2 module"
-  value       = module.my_ec2_instances[*].ec2_outputs  # Replace with actual module output variable names
+  value       = module.my_ec2_instances[*].instance_id  # Replace with actual module output variable names
   depends_on  = [module.my_ec2_instances]
 }
 
 output "rds_outputs" {
   description = "Outputs from the RDS module"
-  value       = module.my_rds_instance[*].rds_outputs  # Replace with actual module output variable names
+  value       = module.my_rds_instance[*].rds_endpoint  # Replace with actual module output variable names
   depends_on  = [module.my_rds_instance]
 }
 
 output "s3_outputs" {
   description = "Outputs from the S3 module"
-  value       = module.my_s3_bucket[*].s3_outputs  # Replace with actual module output variable names
+  value       = module.my_s3_bucket[*].bucket_name  # Replace with actual module output variable names
   depends_on  = [module.my_s3_bucket]
 }
